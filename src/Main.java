09841a01f8ce3d7b2b11f8a70a84ea2025dc9380
1Steveee <truckersteve781@gmail.com>
2022-12-05 21:11:28 -0500

    Merge remote-tracking branch 'origin/master'

--- src/Main.java
+++ src/Main.java
@@ -5,53 +5,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int principal;
-        double annualInterestRate;
-        int loanTerm;
-        String monthlyPayments;
-
-        Scanner scanner = new Scanner(System.in);
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
         numberFormat.setCurrency(Currency.getInstance(Locale.US));
 
-
-        while (true) {
-            System.out.print("Principal ($1k-1M): ");
-            principal = scanner.nextInt();
-
-            if (principal >= 1000 && principal <= 1_000_000) {
-                break;
-            }
-            System.out.println("Please enter a value between 1,000 and 1,000,000");
-        }
-
-        while (true) {
-            System.out.print("Annual Interest Rate: ");
-            annualInterestRate = scanner.nextDouble();
-            if (annualInterestRate >=1 && annualInterestRate <= 30) {
-                break;
-            }
-            System.out.println("Enter Correct Annual Interest Rate");
-        }
-
-        while (true) {
-            System.out.print("Period (Years): ");
-            loanTerm = scanner.nextInt();
-
-            if (loanTerm >= 1 && loanTerm <= 30) {
-                break;
-            }
-            System.out.println("Enter the correct years");
-
-        }
-
-
+        int principal = (int) readNumber("Principal ($1k-1M): ", 1000, 1000000);
+        double annualInterestRate = readNumber("Annual Interest Rate: ", 1,30);
+        int loanTerm = (int) readNumber("Period (Years): ", 1,30);
 
         double mortgage = calculateMortgage(principal,annualInterestRate, loanTerm);
-        monthlyPayments = numberFormat.format(mortgage);
+        String monthlyPayments = numberFormat.format(mortgage);
 
         System.out.println("Mortgage: " + monthlyPayments);
-
     }
 
     public static double calculateMortgage(int principal, double annualInterestRate, int loanTerm) {
@@ -63,5 +27,18 @@ public class Main {
                 /(Math.pow((1 + monthlyInterestRate), loanTerm) - 1);
     }
 
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
 
+            if (value >= min && value <= max) {
+                break;
+            }
+            System.out.println("Please enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
 }
