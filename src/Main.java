b15dbfda40b5137aa10fc85a4ec99de364fc4378
1Steveee <truckersteve781@gmail.com>
2022-12-11 14:59:11 -0500

    refactored methods and added payment functionality

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,42 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+    final static int MONTHS = 12;
+    final static int PERCENT = 100;
     public static void main(String[] args) {
-        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
-        numberFormat.setCurrency(Currency.getInstance(Locale.US));
-
         int principal = (int) readNumber("Principal ($1k-1M): ", 1000, 1000000);
         double annualInterestRate = readNumber("Annual Interest Rate: ", 1,30);
         int loanTerm = (int) readNumber("Period (Years): ", 1,30);
 
-        double mortgage = calculateMortgage(principal,annualInterestRate, loanTerm);
-        String monthlyPayments = numberFormat.format(mortgage);
+        printMonthlyPayment(principal, annualInterestRate, loanTerm);
+        printPaymentSchedule(principal, annualInterestRate, loanTerm);
 
-        System.out.println("Mortgage: " + monthlyPayments);
+    }
+
+    private static void printMonthlyPayment(int principal, double annualInterestRate, int loanTerm) {
+        double mortgage = calculateMortgage(principal, annualInterestRate, loanTerm);
+
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
+        String monthlyPayments = numberFormat.format(mortgage);
+        numberFormat.setCurrency(Currency.getInstance(Locale.US));
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + monthlyPayments);
+    }
+
+    private static void printPaymentSchedule(int principal, double annualInterestRate, int loanTerm) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("-----------------");
+        for (int month = 1; month <= loanTerm * MONTHS; month++) {
+
+            double balance = calculateBalance(principal, annualInterestRate, loanTerm, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
     }
 
     public static double calculateMortgage(int principal, double annualInterestRate, int loanTerm) {
-        final int MONTHS = 12;
-        final int PERCENT = 100;
         double monthlyInterestRate = annualInterestRate/PERCENT/MONTHS;
         loanTerm = loanTerm * MONTHS;
         return principal * (monthlyInterestRate * Math.pow((1 + monthlyInterestRate), loanTerm))
@@ -40,5 +59,15 @@ public class Main {
             System.out.println("Please enter a value between " + min + " and " + max);
         }
         return value;
+    }
+
+    public static double calculateBalance(int principal, double annualInterestRate, int loanTerm, int numOfPaymentsMade) {
+        double monthlyInterestRate = annualInterestRate/PERCENT/MONTHS;
+        int numberOfPayments = loanTerm * MONTHS;
+
+        return principal * (Math.pow(1 + monthlyInterestRate, numberOfPayments)
+                - Math.pow(1 + monthlyInterestRate, numOfPaymentsMade))
+                /(Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+        
     }
 }
